dc9a34e4d93d9f9adaf8086706e62eb763188545
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-08-11 16:59:18 -0700

    quick commit

--- assets/posts/.postGetter.js
+++ assets/posts/.postGetter.js
@@ -1,9 +1,30 @@
 const posts = []
 
-$.getJSON('http://www.raysjournal.com/assets/posts/2.json', post => {
-  posts.push(post)
-})
+// $.getJSON('http://www.raysjournal.com/assets/posts/1.json', post => {
+//   posts.push(post)
+// })
+//
+// $.getJSON('http://www.raysjournal.com/assets/posts/2.json', post => {
+//   posts.push(post)
+// })
+//
+// $.getJSON('http://www.raysjournal.com/assets/posts/3.json', post => {
+//   posts.push(post)
+// })
 
-$.getJSON('http://www.raysjournal.com/assets/posts/3.json', post => {
-  posts.push(post)
-})
+const loadPost = query => {
+  switch (typeof query) {
+    case 'number':
+    console.log(1)
+    break;
+    case 'string':
+    console.log(2)
+    break;
+    default:
+
+  }
+
+  return $.getJSON('http://www.raysjournal.com/assets/posts/' + query + '.json', post => {
+    posts.push(post)
+  })
+}

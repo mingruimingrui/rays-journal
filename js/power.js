3266acd1120ccb99d5ac819051dbe7da8be33a2c
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-08-16 17:26:29 -0700

    scroll button working

--- js/power.js
+++ js/power.js
@@ -5,6 +5,8 @@ $(function() {
   $('.pagination .active a').click(function() {
     return false;
   });
+
+  scrollToOnClick('.scroll-down', $(window).height(), 1000);
 });
 
 
@@ -29,8 +31,26 @@ $(window).resize(function() {
 ==========================================*/
 $(window).scroll(function() {
   if ($(window).scrollTop() > $(window).height()) {
-    console.log('show nav')
+    $('nav.navbar').css('top', '-51px');
   } else {
-    console.log('hide nav')
-  }
+    $('nav.navbar').css('top', 0);
+  };
 });
+
+/*==========================================
+=             HOMEMADE METHODS             =
+==========================================*/
+const fadeIn = (targetName, start, duration) => {
+  $(targetName).css('opacity', 0);
+  setTimeout(() => {
+    $(targetName).fadeTo(duration, 1);
+  }, start)
+}
+
+const scrollToOnClick = (buttonName, targetHeight, duration) => {
+  $(buttonName).click(() => {
+    $('html, body').animate({
+      scrollTop: targetHeight
+    }, duration);
+  })
+}

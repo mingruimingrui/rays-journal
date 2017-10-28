62684ba621de00b5f6ac96267d33de7070a16a56
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-10-28 23:07:26 +0800

    restarted project

--- js/power.js
+++ js/power.js
@@ -2,13 +2,9 @@
 =            DOM IS READY            =
 ====================================*/
 $(function() {
-  $('.pagination .active a').click(function() {
-    return false;
-  });
-
-  // welcome page
-  scrollToOnClick('.scroll-down', $(window).height() + 1, 1000);
-  $('.title-container p')
+    $('.pagination .active a').click(function() {
+        return false;
+    });
 });
 
 
@@ -16,14 +12,12 @@ $(function() {
 =            WINDOW IS LOADED            =
 ========================================*/
 $(window).load(function() {
-  if ($(window).width() > 480) {
-    fadeIn('.title-container p', 100, 600);
-    fadeIn('.gold-button', 300, 600);
-    fadeIn('.link-container', 300, 600);
-    fadeIn('.left-vert-bar', 500, 600);
-    fadeIn('.right-vert-bar', 700, 600);
-  }
-
+  $('.scroll-down').click(event => {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $('.topper').offset().top + $('.topper').height()
+    }, 'slow');
+  })
 });
 
 
@@ -31,13 +25,7 @@ $(window).load(function() {
 =            WINDOW IS RESIZED            =
 =========================================*/
 $(window).resize(function() {
-  if ($(window).width() > 480) {
-    fadeIn('.title-container p', 100, 600);
-    fadeIn('.gold-button', 300, 600);
-    fadeIn('.link-container', 300, 600);
-    fadeIn('.left-vert-bar', 500, 600);
-    fadeIn('.right-vert-bar', 700, 600);
-  }
+
 });
 
 
@@ -45,36 +33,5 @@ $(window).resize(function() {
 =            WINDOW IS SCROLLED            =
 ==========================================*/
 $(window).scroll(function() {
-  if (window.location.pathname === '/') {
-    if ($(window).width() > 480) {
-      if ($(window).scrollTop() > $(window).height()) {
-        $('nav.navbar').css('top', 0);
-      } else {
-        $('nav.navbar').css('top', '-51px');
-      };
-    } else {
-      if ($(window).scrollTop() > $(window).height()) {
-        $('nav.navbar').css('top', 0);
-      } else {
-        $('nav.navbar').css('top', '-51px');
-      };
-    }
-  }
+
 });
-
-/*==========================================
-=             HOMEMADE METHODS             =
-==========================================*/
-const fadeIn = (targetName, start, duration) => {
-  setTimeout(() => {
-    $(targetName).fadeTo(duration, 1);
-  }, start)
-}
-
-const scrollToOnClick = (buttonName, targetHeight, duration) => {
-  $(buttonName).click(() => {
-    $('html, body').animate({
-      scrollTop: targetHeight
-    }, duration);
-  })
-}

d0a17f0c19eebd7ce32da76430649ee13833a4b4
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-10-29 23:32:40 +0800

    added back button

--- js/power.js
+++ js/power.js
@@ -12,12 +12,36 @@ $(function() {
 =            WINDOW IS LOADED            =
 ========================================*/
 $(window).load(function() {
+  // scroll down to article
   $('.scroll-down').click(event => {
     event.preventDefault();
     $('html,body').animate({
       scrollTop: $('.topper').offset().top + $('.topper').height()
     }, 'slow');
-  })
+  });
+
+  // scroll up to top
+  $('.scroll-up').click(event => {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 'slow');
+  });
+
+  // nav-modal controller
+  // menu_modal = $('#menu-modal');
+  //
+  // $('#open-menu-modal').click(event => {
+  //   event.preventDefault();
+  //   menu_modal.css('display', 'block');
+  // });
+  //
+  // $(window).click(event => {
+  //   if (event.target.id === 'menu-modal') {
+  //     menu_modal.css('display', 'none');
+  //   }
+  // });
+
 });
 
 
